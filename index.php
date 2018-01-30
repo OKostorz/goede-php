6b7151afc03e93e1b0ab7cf7ad94114680611142
livv <livv>
2018-01-30 11:17:20 +0100

    no message

--- index.php
+++ index.php
@@ -1,10 +1,35 @@
+<?php
+			date_default_timezone_set("Europe/Amsterdam");
+			$t = date("H:i");
+
+				$message= "";
+					if ($t < "06:00" ) {
+							$message = "Goedenacht";
+
+						} 	else if ($t < "12:00") {
+					 			$message = "Goedemorgen";
+
+							}	else if ($t < "18:00") {
+									$message = "Goedemiddag";
+
+						 		} 	else {
+							 			$message = "Goedenavond";
+			 		}
+
+			 	$message2 = "<br>Het is nu " . $t;
+?>
+		
+		<?php echo "<h2>" . $message . "</h2>"; ?>
+		<?php echo "<h3>" . $message2 . "</h3>"; ?>
+
 <!DOCTYPE html>
-<html lang="en">
+<html>
 <head>
-	<meta charset="UTF-8">
-	<title>Document</title>
+	<title>Goede PHP</title>
+	<link rel="stylesheet" type="text/css" href="style.css">
+	<link href="https://fonts.googleapis.com/css?family=Amatic+SC:700" rel="stylesheet">
+
 </head>
-<body>
-	
+<body class="<?php echo $message;?>">	
 </body>
 </html>
